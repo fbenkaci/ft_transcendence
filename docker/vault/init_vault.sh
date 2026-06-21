7575a8a7d33e0d8c87fc56acf6b2e809e2cd0a76
S1NJED <oooguuh@gmail.com>
2026-06-21 19:59:34 +0200

    fix: disabled logging from script
    Signed-off-by: S1NJED <oooguuh@gmail.com>

--- docker/vault/init_vault.sh
+++ docker/vault/init_vault.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+exec > /dev/null 2>&1
+
 sleep 2
 
 ROOT_TOKEN=$(docker logs transcendence-vault 2>&1 | grep "Root Token:" | awk '{print $3}')
@@ -7,7 +9,7 @@ ROOT_TOKEN=$(docker logs transcendence-vault 2>&1 | grep "Root Token:" | awk '{p
 export VAULT_TOKEN="$ROOT_TOKEN"
 export VAULT_ADDR="http://127.0.0.1:8200"
 
-echo "Root token récupéré: $VAULT_TOKEN"
+# echo "Root token récupéré: $VAULT_TOKEN"
 
 # 2) Créer la policy backend (KV v2)
 docker exec -i transcendence-vault vault policy write backend-policy - <<EOF
@@ -28,4 +30,4 @@ docker exec transcendence-vault vault kv put secret/transcendance \
 docker exec transcendence-vault vault token create -policy=backend-policy -format=json \
   | jq -r ".auth.client_token" > vault_backend_token.txt
 
-echo "Token backend généré et écrit dans vault_backend_token.txt"
+# echo "Token backend généré et écrit dans vault_backend_token.txt"
