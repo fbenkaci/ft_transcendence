49fbad2605a19ff96ecefa0d9075304b203fb612
S1NJED <oooguuh@gmail.com>
2026-06-23 16:51:37 +0200

    refactor: removed TODO comments
    Signed-off-by: S1NJED <oooguuh@gmail.com>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# TODO: a mettre ca dans le vault
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_env_variable("SECRET_KEY")
 print("Vault SECRET_KEY loaded:", SECRET_KEY[:25])
--- docker/vault/init_vault.sh
+++ docker/vault/init_vault.sh
@@ -9,9 +9,7 @@ ROOT_TOKEN=$(docker logs transcendence-vault 2>&1 | grep "Root Token:" | awk '{p
 export VAULT_TOKEN="$ROOT_TOKEN"
 export VAULT_ADDR="http://127.0.0.1:8200"
 
-# echo "Root token récupéré: $VAULT_TOKEN"
 
-# 2) Créer la policy backend (KV v2)
 docker exec -i transcendence-vault vault policy write backend-policy - <<EOF
 path "secret/data/transcendance" {
   capabilities = ["read"]
@@ -22,12 +20,8 @@ path "secret/metadata/transcendance" {
 }
 EOF
 
-# 3) Écrire les secrets initiaux
 docker exec transcendence-vault vault kv put secret/transcendance \
   SECRET_KEY="django-insecure-x%2#kbdwbx@o19a^gb*1&edooqdcu(osxzh11l(fknn7v*0hqb"
 
-# 4) Créer un token limité pour le backend
 docker exec transcendence-vault vault token create -policy=backend-policy -format=json \
   | jq -r ".auth.client_token" > vault_backend_token.txt
-
-# echo "Token backend généré et écrit dans vault_backend_token.txt"
